9621841e5f3393631b2f48233e4cd51b7665a94d
icaro <icarodgl@gmail.com>
2017-11-05 19:23:42 -0200

    organizacao de pastas

--- model/index.php
+++ model/index.php
@@ -2,12 +2,42 @@
 
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+        <meta charset="UTF-8">  
+        <title>RPG Mesa</title>
+        <!-- Compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+
+        <!-- Compiled and minified JavaScript -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+
+        <!--Import Google Icon Font-->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!--Import materialize.css-->   
+        <!--Let browser know website is optimized for mobile-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+        <script type="text/javascript" src="js/materialize.min.js"></script>
     </head>
     <body>
-        <?php
-        echo "Hello World!"
-        ?>
-    </body>
+        
+    <conteiner>
+        <nav>
+            <div class="nav-wrapper">
+                <a href="#" class="brand-logo">Logo</a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Partidas</a></li>
+                    <li><a href="#">Histórias</a></li>
+                </ul>
+            </div>
+            
+        </nav>
+    </conteiner>
+    <conteiner>
+
+    </conteiner>
+    <conteiner>
+
+    </conteiner>
+</body>
 </html>
