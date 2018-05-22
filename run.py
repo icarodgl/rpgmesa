f642fd65377c7c05ecc088d055f6cade5911bb46
icaro.lima <icarodgl@gmail.com>
2018-05-21 22:25:13 -0300

    tentando enganar o heroku

--- run.py
+++ run.py
@@ -6,14 +6,33 @@ from telepot.loop import MessageLoop
 import config
 from controle import Controle
 
+from flask import Flask, request
+from flask_restful import Resource, Api
+from json import dumps
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Ligar(Resource):
+    def get(self):
+        m ={"mensagem":"estou vivo!"}
+        return m
+
 def handle(msg):
     command = msg['text']
     print ('Got command: %s' % command)
     controle = Controle(bot)
     controle.comando(msg)
 
-bot = telepot.Bot('533253560:AAHv5TaR1m3sYd4ek7jR1LSFm1ig0IeOTV8')
-MessageLoop(bot, handle).run_as_thread()
+
 print ('I am listening ...')
-while 1:
-    time.sleep(10)
+#while 1:
+#    time.sleep(10)
+
+
+api.add_resource(Ligar, '/')
+if __name__ == '__main__':
+    bot = telepot.Bot('533253560:AAHv5TaR1m3sYd4ek7jR1LSFm1ig0IeOTV8')
+    MessageLoop(bot, handle).run_as_thread()
+    app.run(port='5002')
