fcee8692600d40227ae09074c607a1bef988ef82
icaro.lima <icarodgl@gmail.com>
2018-05-21 23:20:19 -0300

    funfando

--- run.py
+++ run.py
@@ -6,18 +6,18 @@ from telepot.loop import MessageLoop
 import config
 from controle import Controle
 
-from flask import Flask, request
-from flask_restful import Resource, Api
-from json import dumps
+#from flask import Flask, request
+#from flask_restful import Resource, Api
+#from json import dumps
 
-app = Flask(__name__)
-api = Api(app)
+#app = Flask(__name__)
+#api = Api(app)
 
 
-class Ligar(Resource):
-    def get(self):
-        m ={"mensagem":"estou vivo!"}
-        return m
+#class Ligar(Resource):
+#    def get(self):
+#        m ={"mensagem":"estou vivo!"}
+#        return m
 
 def handle(msg):
     command = msg['text']
@@ -26,13 +26,15 @@ def handle(msg):
     controle.comando(msg)
 
 
-print ('I am listening ...')
-#while 1:
-#    time.sleep(10)
 
 
-api.add_resource(Ligar, '/')
+
+#api.add_resource(Ligar, '/')
 if __name__ == '__main__':
     bot = telepot.Bot('533253560:AAHv5TaR1m3sYd4ek7jR1LSFm1ig0IeOTV8')
     MessageLoop(bot, handle).run_as_thread()
-    app.run(port='5002')
+    print ('I am listening ...')
+    while 1:
+        time.sleep(10)
+
+    #app.run(port='5002')
