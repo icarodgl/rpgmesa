f7526b62af49808734096e443d18606fa50c9af7
icaro <icarodgl@gmail.com>
2018-07-05 12:56:51 -0300

    somente roll funcionando

---
+++ controles/dados.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import random
+import re
+import copy
+class DadoControle():
+
+    def rolarDado(args):
+        dados = args[1:]
+        ldado = []
+        for elem in dados:
+            dado = {
+            "dado":int,
+            "valor":0,
+            "bonus":0
+            }
+            dx = (elem.split('d'))
+            if dx[0] == '':
+                dx.pop(0)
+            if len(dx) > 1:
+                quant = int(dx[0])
+            else:
+                quant = 1
+            if (len(elem.split('-')) > 1 ):
+                dado['bonus'] = int((elem.split('-'))[-1])*-1
+                dado['dado'] = int(re.split(' *, *',(elem.split('-')[-2].split('d')[-1]))[0])
+            elif (len(elem.split('+')) > 1 ):
+                dado['bonus'] = int(elem.split('+')[-1])
+                dado['dado'] = int(re.split(' *, *',(elem.split('+')[-2].split('d')[-1]))[0])
+            else:
+                dado['dado'] = int(dx[-1])
+            for q in range(quant):
+                valor = random.randint(1,dado['dado'])
+                if dado['bonus'] > 0:
+                    valor += dado['bonus']
+                dado['valor'] = valor
+                ldado.append(copy.copy(dado))
+        return ldado
+        
