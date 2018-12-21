aa401c18fc6b21fee511ec4e37b6637c1d937536
Icaro Gavazza <icarodgl@gmail.com>
2018-12-21 18:43:35 -0200

    tecozero

--- controles/zoeira.py
+++ controles/zoeira.py
@@ -1,5 +1,5 @@
 import random
 class ZoeiraControle():
     def teco(self):
-        abobrinhas = ["OLOOOOCO!","LIXO","SEU BATATA"]
-        return abobrinhas[random.randint(0,len(abobrinhas))]
+        abobrinhas = ["OLOOOOCO!","LIXO","SEU BATATA", "Cortei o açucar","Tóxico"]
+        return abobrinhas[random.randint(0,len(abobrinhas)-1)]
