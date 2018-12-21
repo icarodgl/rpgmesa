306cf24394917250c687e91c91a899b7c09fad51
Icaro Gavazza <icarodgl@gmail.com>
2018-12-21 18:17:47 -0200

    foi

--- controles/zoeira.py
+++ controles/zoeira.py
@@ -2,4 +2,4 @@ import random
 class ZoeiraControle():
     def teco(self):
         abobrinhas = ["OLOOOOCO!","LIXO","SEU BATATA"]
-        return random.randint(0,abobrinhas.len())
+        return random.randint(0,len(abobrinhas))
