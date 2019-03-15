80c7e88d831d998ae12d30e9be97874f90b7ca2c
Icaro Gavazza <icarodgl@gmail.com>
2019-03-15 18:03:28 -0300

    configurando django no heroku

--- rpgmesaweb/rpgmesaweb/settings.py
+++ rpgmesaweb/rpgmesaweb/settings.py
@@ -119,6 +119,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+# STATIC_URL = '/static/'
+
+# STATIC_ROOT = '/templates/'
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATIC_ROOT = '/templates/'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
